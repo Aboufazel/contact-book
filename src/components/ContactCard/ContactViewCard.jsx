368a98020091536822663daa8e054a29c8e24a0d
Aboufazel <a.ab2521@gmail.com>
2022-10-05 01:05:16 +0330

    Contact card view complete

--- src/components/ContactCard/ContactViewCard.jsx
+++ src/components/ContactCard/ContactViewCard.jsx
@@ -1,10 +1,23 @@
 import React , {useState} from "react";
 import {UsersDate} from "../DataBase/UsersDate";
-
+import "./ContactCard.style.scss"
 const ContactViewCard = ()=>{
  const [user , setUser] = useState(UsersDate);
     return(
-        <div>ContactCard</div>
+        <div className={"ContactCard"}>
+            {user.map(users =>(
+                <div className={"Card"} key={users.id}>
+                    <div className={"ContactAvatar"}>
+                        <img src={`https://avatars.dicebear.com/api/micah/:${users.id}.svg`}/>
+                    </div>
+                    <div className={"ContactInfo"}>
+                        <div className={"Info_name"}>{users.name}</div>
+                        <div className={"Info_tel"}>{users.phone}</div>
+                    </div>
+                </div>
+            ))
+            }
+        </div>
     )
 }
 
